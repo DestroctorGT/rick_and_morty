4a75b40077d26472782eb11feefb0e7596f5036e
destroctorgt <destroctorgt@gmail.com>
2023-03-22 17:32:06 -0500

    fix: cambios menores en el codigo

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,20 +1,42 @@
 import React from "react";
+import "../Nav/Nav.css";
+import Logo from "../../Assets/Rick-and-Morty.png";
 import SearchBar from "../SearchBar/SearchBar";
 import { Link } from "react-router-dom";
 
 export default function Nav({ onSearch, onRandom }) {
   return (
     <nav>
-      <SearchBar onSearch={onSearch} />
-      <button onClick={onRandom}>Random</button>
+      <ul className="navbar">
+        <li>
+          <img
+            src={Logo}
+            alt="Rick & Morty Logo"
+            className="navbar__logo"></img>
+        </li>
 
-      <Link to="/home">
-        <button>Home</button>
-      </Link>
+        <li>
+          <ul className="navbar navbar__input">
+            <SearchBar onSearch={onSearch} />
+          </ul>
+        </li>
 
-      <Link to="/about">
-        <button>About</button>
-      </Link>
+        <li>
+          <ul className="navbar navbar__buttons">
+            <li>
+              <Link to="/home">
+                <button>Home</button>
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/about">
+                <button>About</button>
+              </Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </nav>
   );
 }
