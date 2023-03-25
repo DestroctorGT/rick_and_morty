2fc4f3e1881788370454bac16d603c94c2b40f60
destroctorgt <destroctorgt@gmail.com>
2023-03-25 11:54:43 -0500

    style: corregido el css del input, los botones y se agregdo dos iconos para los botones

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../SearchBar/SearchBar.css";
 
 export default function SearchBar({ onSearch, onRandom }) {
   const [inputValue, setInputValue] = useState("");
@@ -8,9 +9,23 @@ export default function SearchBar({ onSearch, onRandom }) {
   }
   return (
     <>
-      <input onChange={handleChange} type="search" />
-      <button onClick={() => onSearch(inputValue)}>Agregar</button>
-      <button onClick={onRandom}>Random</button>
+      <button
+        onClick={onRandom}
+        className="searchBar__button searchBar__button--random">
+        <span className="material-symbols-outlined">bolt</span>
+        Random
+      </button>
+      <input
+        className="searchBar__input"
+        onChange={handleChange}
+        type="search"
+      />
+      <button
+        onClick={() => onSearch(inputValue)}
+        className="searchBar__button searchBar__button--add">
+        <span className="material-symbols-outlined">add</span>
+        Agregar
+      </button>
     </>
   );
 }
