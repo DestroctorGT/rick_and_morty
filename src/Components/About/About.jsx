e2510e2a289bea3b60ff36ed9f6ee022aaedbe41
destroctorgt <destroctorgt@gmail.com>
2023-03-29 12:11:49 -0500

    style: diseñado el componente about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import "../About/About.css";
+import about from "../../Assets/about.jpg";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="cardAbout-container">
+      <article className="card-about">
+        <img src={about} alt="about of web developer"></img>
+        <div>
+          <h2>Gino Tapia Barrios</h2>
+          <p>React Developer</p>
+        </div>
+      </article>
+    </div>
+  );
 }
