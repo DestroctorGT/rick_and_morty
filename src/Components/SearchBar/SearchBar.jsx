5a22faac685441f03a362d35f10378cbc5a48d15
destroctorgt <destroctorgt@gmail.com>
2023-03-29 16:40:10 -0500

    style: refactorizado de css legacy a modules

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,31 +1,55 @@
 import { useState } from "react";
-import "../SearchBar/SearchBar.css";
+import { useLocation } from "react-router-dom";
+import styleSearch from "../SearchBar/SearchBar.module.css";
 
 export default function SearchBar({ onSearch, onRandom }) {
   const [inputValue, setInputValue] = useState("");
+
+  const location = useLocation();
+
+  let displaySearchBar = false;
+
+  if (location.pathname === "/" || location.pathname === "/about") {
+    displaySearchBar = false;
+  } else {
+    displaySearchBar = true;
+  }
 
   function handleChange(e) {
     setInputValue(e.target.value);
   }
   return (
-    <>
-      <button
-        onClick={onRandom}
-        className="searchBar__button searchBar__button--random">
-        <span className="material-symbols-outlined">bolt</span>
-        Random
-      </button>
-      <input
-        className="searchBar__input"
-        onChange={handleChange}
-        type="search"
-      />
-      <button
-        onClick={() => onSearch(inputValue)}
-        className="searchBar__button searchBar__button--add">
-        <span className="material-symbols-outlined">add</span>
-        Agregar
-      </button>
-    </>
+    <ul
+      className={
+        !displaySearchBar
+          ? `${styleSearch.disableSearchBar}`
+          : `${styleSearch.searchBar}`
+      }>
+      <li>
+        <button
+          onClick={onRandom}
+          className={`${styleSearch.searchBar__button} ${styleSearch.searchBar__button__random}`}>
+          <span className="material-symbols-outlined">bolt</span>
+          Random
+        </button>
+      </li>
+
+      <li>
+        <input
+          className={styleSearch.searchBar__input}
+          onChange={handleChange}
+          type="search"
+        />
+      </li>
+
+      <li>
+        <button
+          onClick={() => onSearch(inputValue)}
+          className={`${styleSearch.searchBar__button} ${styleSearch.searchBar__button__add}`}>
+          <span className="material-symbols-outlined">add</span>
+          Agregar
+        </button>
+      </li>
+    </ul>
   );
 }
